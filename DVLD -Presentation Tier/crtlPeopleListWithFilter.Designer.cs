8d1755d1a6a1754ad19d8e8ea008efaee77c2006
Yamen Muammar <yamenmuamar@gmail.com>
2026-02-04 22:18:06 +0200

    Add project files.

---
+++ DVLD -Presentation Tier/crtlPeopleListWithFilter.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace DVLD__Presentation_Tier
+{
+    partial class crtlPeopleListWithFilter
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tbFilterInput = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblRecordsCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala Text", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Filter By :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(111, 69);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(246, 28);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // tbFilterInput
+            // 
+            this.tbFilterInput.Location = new System.Drawing.Point(374, 70);
+            this.tbFilterInput.Name = "tbFilterInput";
+            this.tbFilterInput.Size = new System.Drawing.Size(264, 26);
+            this.tbFilterInput.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 113);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1318, 515);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Nirmala Text", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 646);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 28);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Records :";
+            // 
+            // lblRecordsCount
+            // 
+            this.lblRecordsCount.AutoSize = true;
+            this.lblRecordsCount.Font = new System.Drawing.Font("Nirmala Text", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecordsCount.Location = new System.Drawing.Point(106, 646);
+            this.lblRecordsCount.Name = "lblRecordsCount";
+            this.lblRecordsCount.Size = new System.Drawing.Size(36, 28);
+            this.lblRecordsCount.TabIndex = 0;
+            this.lblRecordsCount.Text = "10";
+            // 
+            // crtlPeopleListWithFilter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tbFilterInput);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblRecordsCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "crtlPeopleListWithFilter";
+            this.Size = new System.Drawing.Size(1324, 699);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox tbFilterInput;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblRecordsCount;
+    }
+}
---
+++ DVLD -Presentation Tier/ctrlPersonInformation.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace DVLD__Presentation_Tier
+{
+    partial class ctrlPersonInformation
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbPersonInformation = new System.Windows.Forms.GroupBox();
+            this.lblFullName = new System.Windows.Forms.Label();
+            this.lblCountry = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblPhoneNumber = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.lblNationalNo = new System.Windows.Forms.Label();
+            this.lblPersonID = new System.Windows.Forms.Label();
+            this.btnEditePersonInfo = new System.Windows.Forms.Button();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbPersonInformation.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbPersonInformation
+            // 
+            this.gbPersonInformation.Controls.Add(this.lblFullName);
+            this.gbPersonInformation.Controls.Add(this.lblCountry);
+            this.gbPersonInformation.Controls.Add(this.label10);
+            this.gbPersonInformation.Controls.Add(this.lblPhoneNumber);
+            this.gbPersonInformation.Controls.Add(this.lblAddress);
+            this.gbPersonInformation.Controls.Add(this.lblEmail);
+            this.gbPersonInformation.Controls.Add(this.lblGender);
+            this.gbPersonInformation.Controls.Add(this.lblNationalNo);
+            this.gbPersonInformation.Controls.Add(this.lblPersonID);
+            this.gbPersonInformation.Controls.Add(this.btnEditePersonInfo);
+            this.gbPersonInformation.Controls.Add(this.pbImage);
+            this.gbPersonInformation.Controls.Add(this.label9);
+            this.gbPersonInformation.Controls.Add(this.label8);
+            this.gbPersonInformation.Controls.Add(this.label7);
+            this.gbPersonInformation.Controls.Add(this.label6);
+            this.gbPersonInformation.Controls.Add(this.label5);
+            this.gbPersonInformation.Controls.Add(this.label4);
+            this.gbPersonInformation.Controls.Add(this.label3);
+            this.gbPersonInformation.Controls.Add(this.label2);
+            this.gbPersonInformation.Controls.Add(this.label1);
+            this.gbPersonInformation.Location = new System.Drawing.Point(0, 0);
+            this.gbPersonInformation.Name = "gbPersonInformation";
+            this.gbPersonInformation.Size = new System.Drawing.Size(968, 391);
+            this.gbPersonInformation.TabIndex = 0;
+            this.gbPersonInformation.TabStop = false;
+            this.gbPersonInformation.Text = "Person Information";
+            // 
+            // lblFullName
+            // 
+            this.lblFullName.AutoSize = true;
+            this.lblFullName.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFullName.ForeColor = System.Drawing.Color.Red;
+            this.lblFullName.Location = new System.Drawing.Point(177, 112);
+            this.lblFullName.Name = "lblFullName";
+            this.lblFullName.Size = new System.Drawing.Size(264, 23);
+            this.lblFullName.TabIndex = 2;
+            this.lblFullName.Text = "Yamen wesam abdai mumamr";
+            // 
+            // lblCountry
+            // 
+            this.lblCountry.AutoSize = true;
+            this.lblCountry.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountry.Location = new System.Drawing.Point(589, 256);
+            this.lblCountry.Name = "lblCountry";
+            this.lblCountry.Size = new System.Drawing.Size(73, 20);
+            this.lblCountry.TabIndex = 2;
+            this.lblCountry.Text = "Belgium";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(589, 157);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(99, 20);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "18/04/2005";
+            // 
+            // lblPhoneNumber
+            // 
+            this.lblPhoneNumber.AutoSize = true;
+            this.lblPhoneNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPhoneNumber.Location = new System.Drawing.Point(589, 204);
+            this.lblPhoneNumber.Name = "lblPhoneNumber";
+            this.lblPhoneNumber.Size = new System.Drawing.Size(99, 20);
+            this.lblPhoneNumber.TabIndex = 2;
+            this.lblPhoneNumber.Text = "324999932";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddress.Location = new System.Drawing.Point(177, 299);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(174, 20);
+            this.lblAddress.TabIndex = 2;
+            this.lblAddress.Text = "belgium,9800,deinze";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(177, 253);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(226, 20);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "yamenmummar@gmail.com";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGender.Location = new System.Drawing.Point(177, 207);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(47, 20);
+            this.lblGender.TabIndex = 2;
+            this.lblGender.Text = "Male";
+            // 
+            // lblNationalNo
+            // 
+            this.lblNationalNo.AutoSize = true;
+            this.lblNationalNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNationalNo.Location = new System.Drawing.Point(177, 157);
+            this.lblNationalNo.Name = "lblNationalNo";
+            this.lblNationalNo.Size = new System.Drawing.Size(29, 20);
+            this.lblNationalNo.TabIndex = 2;
+            this.lblNationalNo.Text = "n4";
+            // 
+            // lblPersonID
+            // 
+            this.lblPersonID.AutoSize = true;
+            this.lblPersonID.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPersonID.Location = new System.Drawing.Point(177, 66);
+            this.lblPersonID.Name = "lblPersonID";
+            this.lblPersonID.Size = new System.Drawing.Size(49, 20);
+            this.lblPersonID.TabIndex = 2;
+            this.lblPersonID.Text = "1005";
+            // 
+            // btnEditePersonInfo
+            // 
+            this.btnEditePersonInfo.BackColor = System.Drawing.Color.White;
+            this.btnEditePersonInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditePersonInfo.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnEditePersonInfo.Location = new System.Drawing.Point(751, 60);
+            this.btnEditePersonInfo.Name = "btnEditePersonInfo";
+            this.btnEditePersonInfo.Size = new System.Drawing.Size(153, 28);
+            this.btnEditePersonInfo.TabIndex = 1;
+            this.btnEditePersonInfo.Text = "Edite Person Info";
+            this.btnEditePersonInfo.UseVisualStyleBackColor = false;
+            this.btnEditePersonInfo.Click += new System.EventHandler(this.btnEditePersonInfo_Click);
+            // 
+            // pbImage
+            // 
+            this.pbImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImage.Location = new System.Drawing.Point(737, 138);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(183, 184);
+            this.pbImage.TabIndex = 1;
+            this.pbImage.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Franklin Gothic Demi Cond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(446, 249);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(92, 30);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Country :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Franklin Gothic Demi Cond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(459, 197);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(79, 30);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Phone :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Franklin Gothic Demi Cond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(401, 150);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(137, 30);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Date Of Birth :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Franklin Gothic Demi Cond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(37, 292);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 30);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Address :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Franklin Gothic Demi Cond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(37, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 30);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Email :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Franklin Gothic Demi Cond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(37, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 30);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Gender :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Franklin Gothic Demi Cond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(37, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 30);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "National NO :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Demi Cond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 30);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Name :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Demi Cond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Person ID :";
+            // 
+            // ctrlPersonInformation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gbPersonInformation);
+            this.Name = "ctrlPersonInformation";
+            this.Size = new System.Drawing.Size(968, 391);
+            this.gbPersonInformation.ResumeLayout(false);
+            this.gbPersonInformation.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbPersonInformation;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Button btnEditePersonInfo;
+        private System.Windows.Forms.Label lblFullName;
+        private System.Windows.Forms.Label lblPersonID;
+        private System.Windows.Forms.Label lblNationalNo;
+        private System.Windows.Forms.Label lblCountry;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblPhoneNumber;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblGender;
+    }
+}
