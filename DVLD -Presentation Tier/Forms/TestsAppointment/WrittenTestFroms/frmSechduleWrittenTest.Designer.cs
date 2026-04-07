c1eb11326259284282cf3908e57769b78cfbc460
Yamen Muammar <yamenmuamar@gmail.com>
2026-04-07 16:12:40 +0300

    create form for ctrlWritten

---
+++ DVLD -Presentation Tier/Forms/TestsAppointment/WrittenTestFroms/frmSechduleWrittenTest.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace DVLD__Presentation_Tier.Forms.TestsAppointment.WrittenTestFroms
+{
+    partial class frmSechduleWrittenTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctrlSechduleWrittenTest1 = new DVLD__Presentation_Tier.Controls.SechduleTestsControls.ctrlSechduleWrittenTest();
+            this.SuspendLayout();
+            // 
+            // ctrlSechduleWrittenTest1
+            // 
+            this.ctrlSechduleWrittenTest1.Location = new System.Drawing.Point(12, 12);
+            this.ctrlSechduleWrittenTest1.Name = "ctrlSechduleWrittenTest1";
+            this.ctrlSechduleWrittenTest1.Size = new System.Drawing.Size(616, 805);
+            this.ctrlSechduleWrittenTest1.TabIndex = 0;
+            // 
+            // frmSechduleWrittenTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(633, 822);
+            this.Controls.Add(this.ctrlSechduleWrittenTest1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmSechduleWrittenTest";
+            this.Text = "frmSechduleWrittenTest";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Controls.SechduleTestsControls.ctrlSechduleWrittenTest ctrlSechduleWrittenTest1;
+    }
+}
---
+++ DVLD -Presentation Tier/Forms/TestsAppointment/WrittenTestFroms/frmSechduleWrittenTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DVLD__Presentation_Tier.Controls.SechduleTestsControls;
+
+namespace DVLD__Presentation_Tier.Forms.TestsAppointment.WrittenTestFroms
+{
+    public partial class frmSechduleWrittenTest : Form
+    {
+        private int? appID;
+        private ctrlSechduleWrittenTest.enMode mode;
+        private string applicantFullName;
+        private int LDLAppID;
+        private int testTypeID;
+        private string licenseClassName;
+        private int trail;
+        public frmSechduleWrittenTest()
+        {
+            InitializeComponent();
+        }
+        public frmSechduleWrittenTest(int? appointmentID, ctrlSechduleWrittenTest.enMode mode, string applicantFullName, int LDLAppID, int _testTypeID, string licenseClassName, int trail)
+        {
+            InitializeComponent();
+            appID = appointmentID;
+            this.mode = mode;
+            this.applicantFullName = applicantFullName;
+            this.LDLAppID = LDLAppID;
+            this.testTypeID = _testTypeID;
+            this.licenseClassName = licenseClassName;
+            this.trail = trail;
+            this.ctrlSechduleWrittenTest1._fillUIwithPassedPrameters(appID, mode, applicantFullName, LDLAppID, licenseClassName, trail);
+        }
+    }
+}
